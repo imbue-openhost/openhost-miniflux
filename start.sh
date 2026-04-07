b21d109f73e96fbe65c3598a992f64499a68c450
Andrew Laack <andrew@laack.co>
2026-04-07 12:31:14 -0500

    Set FORCE_REFRESH_INTERVAL to 1 minute (minimum allowed)

--- start.sh
+++ start.sh
@@ -79,7 +79,7 @@ export CREATE_ADMIN=1
 export ADMIN_USERNAME=admin
 export ADMIN_PASSWORD="$ADMIN_PASS"
 export LISTEN_ADDR=0.0.0.0:8080
-export FORCE_REFRESH_INTERVAL=0
+export FORCE_REFRESH_INTERVAL=1
 
 # Derive base URL from OpenHost environment variables
 if [ -n "$OPENHOST_ZONE_DOMAIN" ]; then
